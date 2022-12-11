2aceea656ea2c5c472834f2799e00c701ec965cb
大川貴則 <okawa.t@arsaga.jp>
2022-12-12 00:03:44 +0900

    fix

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,27 +2,29 @@ import { useEffect } from "react";
 import gsap from "gsap";
 import "../assets/css/Loading.css";
 
+// 文字列
 const str = "Loading...";
 
 const Loading = () => {
   // DOMが揃ってから1度だけ実行するために、useEffect内で実行する
   useEffect(() => {
-    const tween = gsap.timeline({ repeat: -1 });
-    tween
-      .to(".str", {
-        y: 20,
-        duration: 0.4,
-        stagger: { each: 0.1 },
-      })
-      .to(".str", {
-        y: 0,
-        duration: 0.4,
-        stagger: { each: 0.1 },
-      });
+    // 連続したアニメーションを実装する為にtimelineオブジェクトを作る。
+    const tl = gsap.timeline({ repeat: -1 });
+
+    tl.to(".str", {
+      y: 20,
+      duration: 0.4,
+      stagger: 0.1,
+    }).to(".str", {
+      y: 0,
+      duration: 0.4,
+      stagger: 0.1,
+    });
   }, []);
 
   return (
     <div className="loading">
+      {/* 分割してspanタグを表示 */}
       {str.split("").map((char, index) => (
         <span className="str" key={index}>
           {char}
